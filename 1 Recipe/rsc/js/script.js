bb866fe6289ff8b42be4321614a3715d2ea43df7
Piotr <MrPiotrMichalski@gmail.com>
2020-09-29 15:52:15 +0200

    commit

--- 1 Recipe/rsc/js/script.js
+++ 1 Recipe/rsc/js/script.js
@@ -1,12 +1,14 @@
 const get_meal = document.getElementById("get_meal");
 const meal = document.getElementById("meal");
 const api_url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+var createMeal;
 
 get_meal.addEventListener('click', () => {
-    fetch('api_url')
+    fetch(api_url)
         .then(res => res.json())
         .then(res => {
             console.log(res.meals[0]);
+            createMeal(res.meals[0]);
     })
     .catch (e => {
         console.warn(e);
